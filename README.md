51e29adafe55603a4e93a90198969d1c6e3a1c4e
Priya-singh-s <priyasingh92782@gmail.com>
2025-01-24 11:34:07 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+## Deploy To Heroku<p align="center">
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/hackercoder24/Ownerhub)<p align="center">
+
+
---
+++ vars.py
@@ -0,0 +1,11 @@
+# Sanjay Kagra 
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+
+API_ID    = os.environ.get("API_ID", "20491966")
+API_HASH  = os.environ.get("API_HASH", "aa1c8f86db7f78fe9bfdd77bb48a5b23")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7589739986:AAEb7DdgtvTsnPsAjDqPsONzhA3atKNXv_o") 
+
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
